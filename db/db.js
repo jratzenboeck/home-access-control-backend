4e3956f7e90f822015c8a52e96539d64454c7217
Roman Sumereder <roman.sumereder@jobs.at>
2019-09-05 20:33:39 +0200

    Make beautiful code

--- db/db.js
+++ db/db.js
@@ -7,5 +7,33 @@ var connection = mysql.createConnection({
     database: 'hackathon'
 });
 
-module.exports = connection;
+function insert(table, content) {
+    return new Promise(((resolve, reject) => {
+        let keys = Object.keys(content);
+        let placeholders = generatePreparedStatementPlaceholders(keys);
+        connection.query(`insert into ${table}(${keys}) values(${placeholders})`, Object.values(content), (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(result.insertId);
+            }
+        });
+    }))
+}
+
+function generatePreparedStatementPlaceholders(values) {
+    let placeholders = '';
+    for (let i = 0; i < values.length; i++) {
+        placeholders += '?';
+        if (i < values.length - 1) {
+            placeholders += ','
+        }
+    }
+    return placeholders;
+}
+
+module.exports = {
+    connection,
+    insert
+};
 
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var decodeBase64Image = require('../http/images');
 var router = express.Router();
 var path = require('path');
 var fs = require('fs');
-var connection = require('../db/db');
+var db = require('../db/db.js');
 
 const { Canvas, Image, ImageData } = canvas;
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
@@ -21,14 +21,6 @@ router.get('/', function (req, res, next) {
 router.post('/users', async function (req, res, next) {
     let name = req.body.name;
     let img64 = req.body.image;
-    let nrOfImages = 0;
-    connection.query('select count(*) as nrOfImages from  images join users on images.user_id = users.id where users.name = ?', name, (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            nrOfImages = result;
-        }
-    });
 
     const imageBuffer = decodeBase64Image(img64);
     fs.writeFile(path.join(__dirname, `../public/images/${name}.png`), imageBuffer.data, function (err) {
@@ -38,8 +30,6 @@ router.post('/users', async function (req, res, next) {
     await faceapi.nets.faceLandmark68Net.loadFromDisk(path.join(__dirname, '../public/models'));
     await faceapi.nets.faceRecognitionNet.loadFromDisk(path.join(__dirname, '../public/models'));
 
-    // load the image
-    // TODO: Replace the static use of the image with the loaded image
     const img = await canvas.loadImage(path.join(__dirname, `../public/images/${name}.png`));
 
     // detect the faces with landmarks
@@ -47,15 +37,12 @@ router.post('/users', async function (req, res, next) {
     if (results !== undefined) {
         const descriptors = results.descriptor;
         const descriptorStr = descriptors.join(',');
-        console.log(descriptorStr);
 
-        connection.query('insert into users(name) values(?)', name, (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                insertImage(result.insertId, 'image2.jpeg', descriptorStr);
-            }
-        });
+        db.insert('users', { name })
+            .then((userId) =>
+                db.insert('images', { user_id: userId, filename: `${name}.png` }))
+            .then((imageId) =>
+                db.insert('image_descriptors', { image_id: imageId, descriptors: descriptorStr }));
 
         return res.json(200);
     }
@@ -66,8 +53,19 @@ router.post('/entry', function (req, res, next) {
 
 });
 
+function getNrOfImagesForUser(name) {
+    db.query('select count(*) as nrOfImages from  images join users on images.user_id = users.id where users.name = ?', name, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            nrOfImages = result[0].nrOfImages;
+            console.log(result[0].nrOfImages);
+        }
+    });
+}
+
 function insertImage(userId, filename, descriptors) {
-    connection.query('insert into images(user_id, filename) values(?, ?)',
+    db.query('insert into images(user_id, filename) values(?, ?)',
         [userId, filename],
         (err, result) => {
             if (err) {
@@ -79,7 +77,7 @@ function insertImage(userId, filename, descriptors) {
 }
 
 function insertDescriptor(imageId, descriptors) {
-    connection.query('insert into image_descriptors(image_id, descriptors) values(?, ?)',
+    db.query('insert into image_descriptors(image_id, descriptors) values(?, ?)',
         [imageId, descriptors],
         (err, result) => {
             if (err) {
