697e4933a1921b8a57c741622ae5d3b447c66a68
Jürgen Ratzenböck <juergen.ratzenboeck@jobs.at>
2019-09-05 23:59:32 +0200

    Find out whether the person on the image submitted should be authenticated

--- routes/index.js
+++ routes/index.js
@@ -1,76 +1,83 @@
-var canvas = require("canvas");
-var faceapi = require('face-api.js');
+var canvas = require('canvas')
+var faceapi = require('face-api.js')
 
-var express = require('express');
-var images = require('../http/images');
-var router = express.Router();
-var path = require('path');
-var fs = require('fs');
-var db = require('../db/db');
-var faceRecognition = require('../lib/face-recognition');
+var express = require('express')
+var images = require('../http/images')
+var router = express.Router()
+var path = require('path')
+var fs = require('fs')
+var db = require('../db/db')
+var faceRecognition = require('../lib/face-recognition')
 
-const { Canvas, Image, ImageData } = canvas;
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+const {Canvas, Image, ImageData} = canvas
+faceapi.env.monkeyPatch({Canvas, Image, ImageData})
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
-});
+  res.render('index', {title: 'Express'})
+})
 
 router.post('/users', async function (req, res, next) {
-    let name = req.body.name;
-    let img64 = req.body.image;
+  let name = req.body.name
+  let img64 = req.body.image
 
-    const imageBuffer = images.decodeBase64Image(img64);
-    images.writeToFile(imageBuffer.data, name);
+  const imageBuffer = images.decodeBase64Image(img64)
+  images.writeToFile(imageBuffer.data, `${name}.png`)
 
-    await faceRecognition.loadModels('../public/models');
-    const img = await canvas.loadImage(path.join(__dirname, `../public/images/${name}.png`));
+  await faceRecognition.loadModels('../public/models')
+  const img = await images.buildCanvasFromImage(`${name}.png`)
 
-    // detect the faces with landmarks
-    const results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-    if (results !== undefined) {
-        const descriptors = results.descriptor;
-        const descriptorStr = descriptors.join(',');
+  // detect the faces with landmarks
+  const results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+  if (results !== undefined) {
+    const descriptors = results.descriptor
+    const descriptorStr = descriptors.join(',')
 
-        db.insert('users', { name })
-            .then((userId) =>
-                db.insert('images', { user_id: userId, filename: `${name}.png` }))
-            .then((imageId) =>
-                db.insert('image_descriptors', { image_id: imageId, descriptors: descriptorStr }));
+    db.insert('users', {name})
+      .then((userId) =>
+        db.insert('images', {user_id: userId, filename: `${name}.png`}))
+      .then((imageId) =>
+        db.insert('image_descriptors', {image_id: imageId, descriptors: descriptorStr}))
 
-        return res.json(200);
-    }
-    return res.json(404);
-});
+    return res.json(200)
+  }
+  return res.json(404)
+})
 
 router.post('/enter', async function (req, res, next) {
-    const imageBuffer = images.decodeBase64Image(req.body.image);
-    images.writeToFile(imageBuffer.data, 'file.png');
+  const imageBuffer = images.decodeBase64Image(req.body.image)
+  images.writeToFile(imageBuffer.data, 'file.png')
 
-    // Load models
-    await faceRecognition.loadModels('../public/models');
-    const img = await images.buildCanvasFromImage('file.png');
-    // detect the faces with landmarks
-    const results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
-    if (results !== undefined) {
-        const descriptors = results.descriptor;
-        console.log(descriptors)
-        // Get all image descriptors from the db
-        db.all('image_descriptors', ['*']).then((existingDescriptors) => {
-            const mappedDescriptors = existingDescriptors.map(
-              entry => new Float32Array(entry.descriptors.split(','))
-            );
-            const distance = faceRecognition.computeSmallestEuclideanDistance(descriptors, mappedDescriptors);
-            console.log(distance)
-        });
-        // Compute the euclidean distance to each of them
-        // Take the minimum distance
-        // Check whether it exceeds threshold
-        return res.json(200);
-    }
-    return res.json(404);
-});
+  // Load models
+  await faceRecognition.loadModels('../public/models')
+  const img = await images.buildCanvasFromImage('file.png')
+  // detect the faces with landmarks
+  const results = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+  if (results !== undefined) {
+    const descriptors = results.descriptor
+    // Get all image descriptors from the db
+    db.all('image_descriptors', ['*']).then((existingDescriptors) => {
+      computeDistance(descriptors, existingDescriptors).then((distance) => {
+        return res.json({authenticated: distance <= 0.5})
+      })
+    })
+    // Compute the euclidean distance to each of them
+    // Take the minimum distance
+    // Check whether it exceeds threshold
+  } else {
+    return res.json(404)
+  }
+})
+
+function computeDistance (descriptors, existingDescriptors) {
+  return new Promise((resolve, reject) => {
+    const mappedDescriptors = existingDescriptors.map(
+      entry => new Float32Array(entry.descriptors.split(','))
+    )
+    const distance = faceRecognition.computeSmallestEuclideanDistance(descriptors, mappedDescriptors)
+    resolve(distance)
+  })
+}
 
 /*function getFaceDetectorOptions() {
     return net === faceapi.nets.ssdMobilenetv1
@@ -80,4 +87,4 @@ router.post('/enter', async function (req, res, next) {
                 : new faceapi.MtcnnOptions({ minFaceSize, scaleFactor })
         )
 }*/
-module.exports = router;
+module.exports = router
