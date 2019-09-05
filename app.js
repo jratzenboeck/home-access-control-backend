676ad5d81ddf6d526a0cce06e8ef275f91b58db6
Roman Sumereder <roman.sumereder@jobs.at>
2019-09-05 17:52:01 +0200

    Commit first tries to face detection

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var logger = require('morgan');
 var faceapi = require('face-api.js');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,9 +18,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-console.log(faceapi.nets);
+//console.log(faceapi.nets);
 
 const { Canvas, Image, ImageData } = canvas;
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
